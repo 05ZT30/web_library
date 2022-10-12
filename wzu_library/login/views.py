a74bd2560a07c9a967966b24e61372adbc8001f2
05ZT30 <molly.zheng.work@outlook.com>
2022-10-12 20:32:12 +0800

    add index url to head box

--- wzu_library/login/views.py
+++ wzu_library/login/views.py
@@ -14,11 +14,13 @@ def login_view(request):
         if user is not None:
             login(request, user)
             message = '登录成功！'
+            #现在chat登录之后也会跳转到主页，chat自身检查并回到刚才的页面这个url失效
+            #是否可以通过弹窗让用户选择是否需要跳转到主页，这样子就可以让chat跳转到源页面了
             return render(request, 'index.html', locals())
         else:
             message = '用户名或密码错误！'
     else:
-        return render(request, 'login.html', locals())
+        return render(requesivt, 'login.html', locals())
 
     # login_form = forms.UserForm()
     return render(request, 'login.html')
