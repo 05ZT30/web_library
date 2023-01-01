8882dadb1d0a96e322588de8a9ed5fff50230e25
05ZT30 <molly.zheng.work@outlook.com>
2023-01-01 14:11:54 +0800

    一些前端界面优化

--- wzu_library/login/urls.py
+++ wzu_library/login/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from login.views import *
 
 urlpatterns = [
-    url(r"^login/", login , name="login"),
-    url(r"^register/", register , name="register"),
+    #url(r"^login/", login , name="login"),
+    #url(r"^register/", register , name="register"),
 ]
