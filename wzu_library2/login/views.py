ad6f0b17d34044541d80032c71ff6f13104f2acf
05ZT30 <molly.zheng.work@outlook.com>
2023-01-13 10:15:53 +0800

    数据库搞崩了，开始迁移数据到LivingLibrary文件夹中，目前LL刚建好

---
+++ wzu_library2/login/views.py
@@ -0,0 +1,89 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import UserForm,UserRegisterForm
+from django.contrib.auth.models import Group
+from login.models import User 
+
+users_in_manager_group = Group.objects.get(name="Manager").user_set.all()
+users_in_teacher_group = Group.objects.get(name="Teacher").user_set.all()
+users_in_student_group = Group.objects.get(name="Student").user_set.all()
+# Create your views here.
+
+
+def login_view(request):
+    if request.session.get('is_login',None):
+       return render(request, 'index.html', locals())
+    if request.method == 'POST':
+        login_form = UserForm(request.POST)
+        message = '请检查填写的内容！'
+        if login_form.is_valid():
+            username = login_form.cleaned_data.get('username')
+            password = login_form.cleaned_data.get('password')
+
+            try:
+                user = User.objects.get(username=username)
+            except :
+                message = '用户不存在！'
+                return render(request, 'login.html', locals())
+
+            if user.password == password:
+                #TODO 增加密码加密
+                request.session['is_login'] = True
+                request.session['username'] = username
+                return render(request, 'index.html', locals())
+            else:
+                message = '密码不正确！'
+                return render(request, 'login.html', locals())
+        else:
+            return render(request, 'login.html', locals())
+    else:
+        login_form = UserForm()
+    return render(request, 'login.html', locals())
+
+
+def register_view(request):
+    if request.session.get('is_login', None):
+        return redirect('/index/')
+
+    if request.method == 'POST':
+        register_form = UserRegisterForm(request.POST)
+        message = "请检查填写的内容！"
+        if register_form.is_valid():
+            username = register_form.cleaned_data.get('username')
+            password1 = register_form.cleaned_data.get('password1')
+            password2 = register_form.cleaned_data.get('password2')
+            group_select = register_form.cleaned_data.get('group')
+            if password1 != password2:
+                message = '两次输入的密码不同！'
+                return render(request, 'register.html', locals())
+            else:
+                samename_user = User.objects.filter(username=username)
+                if samename_user:
+                    message = '该用户已经存在'
+                    return render(request, 'register.html', locals())
+                
+                try:
+                    group = Group.objects.get(name = group_select)
+                except:
+                    Group.objects.create(name = group_select)
+                    group = Group.objects.get(name = group_select)
+                    
+                new_user = User()
+                new_user.username = username
+                new_user.password = hash(password1)
+                # new_user.group = group
+                new_user.save()
+                user = User.objects.get(username=username)
+                group.user_set.add(user)
+                return redirect('/login/')
+        else:
+            return render(request, 'register.html', locals())
+    register_form = UserRegisterForm ()
+    return render(request, 'register.html', locals())
+
+
+def logout_view(request):
+    if not request.session.get('is_login', None):
+        return redirect("/login/")
+    request.session.flush()
+    return redirect("/login/")
---
+++ wzu_library2/wzu_library/settings.py
@@ -0,0 +1,159 @@
+from pathlib import Path
+from re import I
+import os
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-qih&+nj6!bor^&cgs-6qhqjdxh32cy0qlu)_stnx3pci8w)kbt'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+# Application definition
+INSTALLED_APPS = [
+    'simpleui',
+    'import_export',
+
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'chat',
+    'main',
+    'login',
+
+    'bootstrap4',
+    'captcha',
+    'django_private_chat2',
+    'channels',
+    'django_extensions',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'wzu_library.urls'
+LOGIN_REDIRECT_URL = '/login/'
+LOGIN_URL = '/login/'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(BASE_DIR, "templates")
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'wzu_library.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'rm-bp1g64rulfs7e25509o.mysql.rds.aliyuncs.com',
+        'PORT': '3306',
+        'USER': 'zyt',
+        'PASSWORD': 'abc+123456',
+        'NAME': 'LibraryDB',
+    },
+    # 'redis': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+# AUTH_USER_MODEL = 'login'
+
+# Internationalization
+# https://docs.djangoproject.com/en/3.2/topics/i18n/
+
+LANGUAGE_CODE = 'zh-hans'
+
+TIME_ZONE = 'Asia/Shanghai'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = False
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates/static')]
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# simple ui
+SIMPLEUI_LOGO = 'http://www.wzu.edu.cn/__local/0/E8/06/4670119A4E453581250C1D4D924_0AFE2605_4FDA.png'
+
+# 隐藏右侧SimpleUI广告链接和使用分析
+SIMPLEUI_HOME_INFO = False
+SIMPLEUI_ANALYSIS = False
+
+# 修改左侧菜单首页设置
+# SIMPLEUI_HOME_PAGE = '/index/'  # 指向页面
+# SIMPLEUI_HOME_TITLE = '首页' # 首页标题
+# SIMPLEUI_HOME_ICON = 'fa fa-code' # 首页图标
+
+# 设置右上角Home图标跳转链接，会以另外一个窗口打开
+SIMPLEUI_INDEX = '/index/'
+
+# Channels
+ASGI_APPLICATION = 'wzu_library.routing.application'
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer'
+    }
+}
