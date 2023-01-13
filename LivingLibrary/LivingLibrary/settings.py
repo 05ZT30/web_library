f4cfd30145c7c7982017ae4aa3bacedf3781b25a
05ZT30 <molly.zheng.work@outlook.com>
2023-01-13 15:56:28 +0800

    迁移数据库了

--- LivingLibrary/LivingLibrary/settings.py
+++ LivingLibrary/LivingLibrary/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'LivingLibrary.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'rm-bp1g64rulfs7e25509o.mysql.rds.aliyuncs.com',
+        'PORT': '3306',
+        'USER': 'zyt',
+        'PASSWORD': 'abc+123456',
+        'NAME': 'LivingLibraryDB',
+    },
 }
 
 
