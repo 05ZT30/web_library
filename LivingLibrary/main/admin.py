adc1ac1b6a62e8cc665a24a85ae71aa8de9d7d92
05ZT30 <molly.zheng.work@outlook.com>
2023-04-25 18:45:05 +0800

    main app的管理更改

--- LivingLibrary/main/admin.py
+++ LivingLibrary/main/admin.py
@@ -1,8 +1,25 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 # Register your models here.
 
 from .models import *
 
+class RollAdmin(BaseUserAdmin):
+    list_display = ('id', 'title', 'picture',)
+    list_per_page = 20
+    list_filter = ('title',)
+    fieldsets = (
+        ('基本信息', {'fields': ('id', 'title', 'picture')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ( 'title', 'picture',),
+        }),
+    )
+    # del BaseUserAdmin.search_fields
+    ordering = ['id']
+    filter_horizontal = ()
+
 admin.site.register(News)
-admin.site.register(roll)
+admin.site.register(roll,RollAdmin)
--- LivingLibrary/main/models.py
+++ LivingLibrary/main/models.py
@@ -9,6 +9,10 @@ class News(models.Model):
     content = RichTextField(verbose_name='正文内容',config_name='default',default='abc')#config_name指定ckeditor配置文件，不指定就使用default
     def __str__(self):
         return self.title
+    class Meta:
+        # permissions =
+        verbose_name = "新闻"
+        verbose_name_plural = "所有新闻"
 
 class roll(models.Model):
     title = models.CharField(max_length=254,verbose_name="标题")
@@ -17,3 +21,7 @@ class roll(models.Model):
 
     def __str__(self):
         return self.title
+    class Meta:
+        # permissions =
+        verbose_name = "轮播图"
+        verbose_name_plural = "所有轮播图"
