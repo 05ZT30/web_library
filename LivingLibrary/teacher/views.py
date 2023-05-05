062e3501e1de42ca4be6073b75b2cc9e65afed3e
05ZT30 <molly.zheng.work@outlook.com>
2023-05-06 07:16:25 +0800

    cbb

--- LivingLibrary/teacher/views.py
+++ LivingLibrary/teacher/views.py
@@ -1,10 +1,24 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from login.models import MyUser
+
 
 # Create your views here.
 def index(request):
 #      return render(request, 'teacher.html', locals())
-
-# def teacher_view(request):
     teachers = MyUser.objects.filter(is_teacher=True)
-    return render(request, 'teacher.html', {'teachers': teachers})
+# def teacher_view(request):
+    page = int(request.GET.get('page',1))
+    page_szie = 20
+    start = (page -1)* page_szie
+    end =page *page_szie
+    teachers = MyUser.objects.filter(is_teacher=True)[start:end]
+    return render(request, 'teacher.html', {"teachers":teachers})
+
+
+def teacher_view(request, category='all'):
+    if category == 'all':
+        teachers = MyUser.objects.filter(is_teacher=True)
+    else:
+        teachers = MyUser.objects.filter(is_teacher=True, subject=category)
+    return render(request, 'teacher.html', {'teachers': teachers, 'category': category})
