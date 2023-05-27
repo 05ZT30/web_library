3ed4566375f3266415b9a7cec9b717b7abf568ac
05ZT30/ <molly.zheng.work@outlook.com>
2023-05-27 18:57:43 +0800

    一些错误更正

--- LivingLibrary/chat2/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-# chat/views.py
-from django.shortcuts import render
-
-def index(request):
-    return render(request, "chat2/index.html")
